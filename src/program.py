180051bda6f3b3bd5143214841245c2ae7e696df
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-01-26 09:35:48 -0300

    feat: booleans and condition and -  or

--- src/program.py
+++ src/program.py
@@ -40,3 +40,52 @@ print("calculator program")
 first_number = input("first number: ")
 second_number = input("second number: ")
 print(int(first_number) + int(second_number))
+
+
+# lógica booliana em python
+
+a = 93
+b = 27
+if a >= b:
+    print(a)
+else:
+    print(b)
+
+c = 20
+d = 10
+if c >= d:
+    print("a is greater than or equal to b")
+elif c == d:
+    print("a is equal to b")
+
+
+#  lógica condicional aninhada
+
+a = 16
+b = 25
+c = 27
+if a > b:
+    if b > c:
+        print("a is greater than b and b is greater than c")
+    else:
+        print("a is greater than b and less than c")
+elif a == b:
+    print("a is equal to b")
+else:
+    print("a is less than b")
+
+
+# operadores boolianos and e or.
+
+a = 23
+b = 34
+# para que a expressão seja True, pelo menos uma das subexpressões deve ser verdadeira.
+if a == 34 or b == 34:
+    print(a + b)
+
+
+a = 23
+b = 34
+# As duas condições na expressão de teste devem ser verdadeiras para que a expressão de teste inteira seja avaliada como True.
+if a == 34 and b == 34:
+    print(a + b)
