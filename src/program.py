b4f2dba8977aa5404a8cdf758601e771a01ab686
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2023-01-31 09:53:12 -0300

    formatting strings

--- src/program.py
+++ src/program.py
@@ -89,7 +89,7 @@ if a == 34 and b == 34:
     print(a + b)
 
 
-# MÉTODOS DE CADEIA DE CARACTERES
+# MÉTODOS DE CADEIA DE CARACTERES <<<<<<<<<<<
 
 heading = "temperatures and facts about the moon"
 heading.title()
@@ -171,3 +171,42 @@ moon_facts = [
 ]
 '\n'.join(moon_facts)
 'The Moon is drifting away from the Earth.\nOn average, the Moon is moving about 4cm every year'
+
+
+# FORMATAÇÃO DO SINAL DE PORCENTAGEM
+
+mass_percentage = "1/6"
+print("On the Moon, you would weigh about %s of your weight on Earth" %
+      mass_percentage)
+'On the Moon, you would weigh about 1/6 of your weight on Earth'
+
+print("""Both sides of the %s get the same amount of sunlight,
+... but only one side is seen from %s because
+... the %s rotates around its own axis when it orbits %s.""" % ("Moon", "Earth", "Moon", "Earth"))
+
+
+# .format() usa chaves ({}) como espaços reservados em uma cadeia de caracteres e usa a atribuição de variável para substituir o texto.
+mass_percentage = "1/6"
+print("On the Moon, you would weigh about {} of your weight on Earth".format(
+    mass_percentage))
+'On the Moon, you would weigh about 1/6 of your weight on Earth'
+
+
+print("""You are lighter on the {0}, because on the {0} 
+... you would weigh about {1} of your weight on Earth""".format("Moon", mass_percentage))
+'You are lighter on the Moon, because on the Moon you would weigh about 1/6 of your weight on Earthc'
+
+print("""You are lighter on the {moon}, because on the {moon} 
+... you would weigh about {mass} of your weight on Earth""".format(moon="Moon", mass=mass_percentage))
+'You are lighter on the Moon, because on the Moon you would weigh about 1/6 of your weight on Earth'
+
+
+# cadeias de caracteres f
+
+print(
+    f"On the Moon, you would weigh about {mass_percentage} of your weight on Earth")
+'On the Moon, you would weigh about 1/6 of your weight on Earth'
+
+print(
+    f"On the Moon, you would weigh about {round(100/6, 1)}% of your weight on Earth")
+'On the Moon, you would weigh about 16.7% of your weight on Earth'
